935474b43e61f420de82412d4703bb54b181bea6
Uzytkownik <uzytkownik@kursGIT.pl>
2023-10-14 16:41:41 +0200

    add models

--- hollymovies/viewer/models.py
+++ hollymovies/viewer/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.db.models import (
+  DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
+  Model, TextField
+)
 
-# Create your models here.
+class Genre(Model):
+    name = CharField(max_length=128)
+    
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class Movie(Model):
+  title = CharField(max_length=128)
+  genre = ForeignKey(Genre, on_delete=DO_NOTHING)
+  rating = IntegerField()
+  released = DateField()
+  description = TextField()
+  created = DateTimeField(auto_now_add=True)
+
+  def __str__(self) -> str:
+     return f'{self.title} | {self.released} | {self.genre}'
+  
